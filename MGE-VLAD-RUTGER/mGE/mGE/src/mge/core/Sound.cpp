218a883da141ca760d28cf6a6811a68604174ecc
Kameloter <kameloter@hotmail.nl>
2016-03-17 14:03:27 +0100

    Revert "Revert "v0.11""
    This reverts commit b7033ddee353f77efe158f431b0f4ae63a77555c.

--- MGE-VLAD-RUTGER/mGE/mGE/src/mge/core/Sound.cpp
+++ MGE-VLAD-RUTGER/mGE/mGE/src/mge/core/Sound.cpp
@@ -1,11 +1,36 @@
-//#include "Sound.h"
-//
-//Sound::Sound()
-//{
-//    //ctor
-//}
-//
-//Sound::~Sound()
-//{
-//    //dtor
-//}
+#include "Sound.h"
+
+Sound::Sound()
+{
+
+}
+
+Sound::~Sound()
+{
+
+}
+
+bool Sound::Load(std::string fileName)
+{
+	if (!buffer.loadFromFile(fileName))
+	{
+		std::cout << " WRONG SOUND NAME !" << std::endl;
+		return false;
+	}
+	else 
+	{
+		sound.setBuffer(buffer);
+		return true;
+	}
+	
+}
+
+void Sound::Play(bool stop)
+{
+	sound.play();
+}
+
+void Sound::setLooping(bool value)
+{
+	sound.setLoop(value);
+}
